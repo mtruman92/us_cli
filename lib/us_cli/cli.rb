8f8f35bf5e8bcb23caafde73f829513f45b1ff4b
Mickae Truman <mtruman@nyit.edu>
2019-04-14 02:45:26 +0000

    updated cli

--- lib/us_cli/cli.rb
+++ lib/us_cli/cli.rb
@@ -1,7 +1,136 @@
-class UsCli::CLI
+#class UsCli::CLI
   
-  def call
-    puts "Today's History:"
-  end 
-  
+  def help
+  help = <<-HELP
+  Welcome to United States Facts Portal. Please utilize the following commands:
+  - help : displays this help message
+  - list : displays a list of the United States that you can get more information on
+  - details : displays details about the state you chose 
+  - exit : exits the portal\n
+HELP
+
+  #puts help
 end
+
+#help 
+
+
+states = [ 
+"Alabama",
+"Alaska",
+"Arizona",
+"Arkansas",
+"California",
+"Colorado",
+"Connecticut",
+"Delaware",
+"Florida",
+"Georgia",
+"Hawaii",
+"Idaho",
+"Illinois",
+"Indiana",
+"Iowa",
+"Kansas",
+"Kentucky",
+"Louisiana",
+"Maine",
+"Maryland",
+"Massachusetts",
+"Michigan",
+"Minnesota",
+"Mississippi",
+"Missouri",
+"Montana",
+"Nebraska",
+"Nevada",
+"New Hampshire",
+"New Jersey",
+"New Mexico",
+"New York",
+"North Carolina",
+"North Dakota",
+"Ohio",
+"Oklahoma",
+"Oregon",
+"Pennsylvania",
+"Rhode Island",
+"South Carolina",
+"South Dakota",
+"Tennessee",
+"Texas",
+"Utah",
+"Vermont",
+"Virginia",
+"Washington",
+"West Virginia",
+"Wisconsin",
+"Wyoming \n "]
+
+def greeting
+puts "Welcome to The United States Facts Portal! \n "
+end
+
+greeting 
+
+def list(states)
+  states.each_with_index {|s, index|
+  puts "#{index+1}. #{s}"}&&"\n"
+end
+
+ 
+list(states) 
+
+def details(states)
+  puts "Hello, please choose a state:\n "
+  input = gets.chomp
+  if (1..50).to_a.include?(input.to_i) 
+  puts "\n"
+    puts "Hi, you've chosen #{states[input.to_i - 1]} \n"
+    #print_us(unitedstates)
+    elsif states.include?"#{states[input.to_i - 1]}"
+    puts "Hi, you've chosen #{input}! \n "
+  else 
+    puts "Invalid input, please try again! \n ".upcase
+  end 
+end 
+
+details(states)
+
+def exit_states
+  puts "Thank you for using The United States Facts portal, please come again!"
+end 
+
+def run(states)
+  #help
+  command = "" 
+  while command  
+  puts "Please enter a command (options include: 
+  - help : displays this help message
+  - list : displays a list of the United States that you can get more information on
+ 
+  - exit : exits the portal\n )"
+   #unless 
+    #puts "Invalid input, please try again"
+    #puts details(states)
+  command = gets.downcase.strip  
+  
+  case command 
+    when 'list'
+      list(states) && details(states)
+      #when 'details'
+        #puts "Hi, you've chosen #{states[input.to_i - 1]}"
+        #details(states)
+      when 'help'
+        help 
+      when 'exit'
+        exit_states
+        break 
+      else 
+        help 
+      end 
+    end 
+  end 
+  run(states)
+  
+#end
--- lib/us_cli/scraper.rb
+++ lib/us_cli/scraper.rb
@@ -3,30 +3,30 @@ require "open-uri"
 
 BASE_PATH = "https://www.history.com/topics/us-states/"
 
-path = BASE_PATH + "florida"
+path = BASE_PATH + "#{@state_name}"
 
 # Scrape details --------------------
 
 file = open(path)
-node = Nokogiri::HTML(file)
+doc = Nokogiri::HTML(file)
 
-puts detail = node.css("div.m-detail--body p")[0].text
+puts detail = doc.css("div.m-detail--body p")[0].text
 
 puts
 
-puts dos = node.css("div.m-detail--body p")[1].text
-puts capital = node.css("div.m-detail--body p")[2].text
-puts pop = node.css("div.m-detail--body p")[3].text
-puts size = node.css("div.m-detail--body p")[4].text
-puts nick_name = node.css("div.m-detail--body p")[5].text
-puts moto = node.css("div.m-detail--body p")[6].text
+puts entry_date = doc.css("div.m-detail--body p")[1].text
+puts capital = doc.css("div.m-detail--body p")[2].text
+puts pop = doc.css("div.m-detail--body p")[3].text
+puts size = doc.css("div.m-detail--body p")[4].text
+puts nick_name = doc.css("div.m-detail--body p")[5].text
+puts moto = doc.css("div.m-detail--body p")[6].text
 
 puts
 puts "Here are the details:"
 puts
 facts = []
 
-detail = node.css("div.m-detail--body ul").each do |fact|
+detail = doc.css("div.m-detail--body ul").each do |fact|
   facts << fact.text
 end
 
@@ -40,10 +40,10 @@ require "open-uri"
 
 path = "https://www.history.com/topics/us-states"
 file = open(path)
-node = Nokogiri::HTML(file)
+doc = Nokogiri::HTML(file)
 
 states = []
-node.css(".m-ellipsis--text").each do |state|
+doc.css(".m-ellipsis--text").each do |state|
   states << state.text
 end
 
